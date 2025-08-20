671c056fd8514b747c2723bd394f9c82017da520
Hervin-avio <jabarjuara44.16@gmail.com>
2025-08-20 15:58:11 +0700

    tambah function

--- tugas-logika.php
+++ tugas-logika.php
@@ -1,18 +1,24 @@
 <?php
 
-echo "Masukkan nilai: ";
-$nilai = (int) trim(fgets(STDIN)); 
-
-if ($nilai >= 0 && $nilai <= 49) {
-    echo "E\n";
-} elseif ($nilai >= 50 && $nilai <= 59) {
-    echo "D\n";
-} elseif ($nilai >= 60 && $nilai <= 74) {
-    echo "C\n";
-} elseif ($nilai >= 75 && $nilai <= 84) {
-    echo "B\n";
-} elseif ($nilai >= 85 && $nilai <= 100) {
-    echo "A\n";
-} else {
-    echo "Nilai tidak valid\n";
+function getGrade(int $nilai): string {
+    if ($nilai >= 0 && $nilai <= 49) {
+        return "E";
+    } elseif ($nilai >= 50 && $nilai <= 59) {
+        return "D";
+    } elseif ($nilai >= 60 && $nilai <= 74) {
+        return "C";
+    } elseif ($nilai >= 75 && $nilai <= 84) {
+        return "B";
+    } elseif ($nilai >= 85 && $nilai <= 100) {
+        return "A";
+    } else {
+        return "Nilai tidak valid";
+    }
 }
+
+echo "Masukkan nilai: ";
+$nilai = (int) trim(fgets(STDIN));
+
+$hasil = getGrade($nilai);
+
+echo "Grade: $hasil\n";
